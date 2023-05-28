6ff6c6c94f44dcecc8c75877116e3c536527795a
Gary <garylow2001@gmail.com>
2023-05-28 13:51:22 +0800

    Added Dashboard and Auth

---
+++ kiasu_grocer/src/pages/Dashboard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Dashboard = () => {
+    const navigate = useNavigate();
+    const goToAuth = () => navigate('/auth');
+
+    return <div>
+        <h1>
+            This is the Dashboard
+        </h1>
+        <h2>
+            <button onClick={goToAuth}>
+                Click here to Login
+            </button>
+        </h2>
+    </div>
+}
+
+export default Dashboard;
