8541cb6bb66d8429c3f8b51ceb9b2d8ae768a810
thamy <thamyouwei@gmail.com>
2023-05-29 15:58:26 +0800

    API

--- kiasu_grocer/src/pages/Dashboard.js
+++ kiasu_grocer/src/pages/Dashboard.js
@@ -4,32 +4,70 @@ import { useNavigate } from "react-router-dom";
 const Dashboard = () => {
     const navigate = useNavigate();
     const goToAuth = () => navigate('/auth');
-    const fetchData = () => {
+
+    const calcDistance = (lat1, lon1, lat2, lon2) => {
+        var R = 6371; // Radius of the earth in km
+        var degLat = degToRad(lat2-lat1);  // deg2rad below
+        var degLon = degToRad(lon2-lon1); 
+        var a = Math.sin(degLat/2) * Math.sin(degLat/2) +
+                Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2)) * 
+                Math.sin(degLon/2) * Math.sin(degLon/2); 
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+        var d = R * c; // Distance in km
+        return d;
+    }
+
+    function degToRad(deg) {
+        return deg * (Math.PI/180)
+    }
+
+    const RESTresponse = () => {
         var requestOptions = {
             method: 'GET',
             redirect: 'follow'
           };
 
-        var markets = fetch("https://data.gov.sg/api/action/datastore_search?resource_id=df586152-d00f-4b15-b667-9e268f1b60df&limit=5", requestOptions)
+        let markets = fetch("https://data.gov.sg/api/action/datastore_search?resource_id=df586152-d00f-4b15-b667-9e268f1b60df&limit=5", requestOptions)
                         .then(response => response.json())
                         .then(result => result.result.records)
-                        .catch(error => console.log('error', error))
                         .then(x => {
-                            x.forEach(element => {
-                                fetch("http://api.positionstack.com/v1/forward?access_key=77dd088611e15e988f32434bfe65a3db&query=" + element.premise_address, requestOptions)
+                            return x.map(element => {
+                                return fetch("http://api.positionstack.com/v1/forward?access_key=77dd088611e15e988f32434bfe65a3db&query=" + element.premise_address, requestOptions)
                                     .then(response => response.json())
-                                    .then(result => console.log(result.data[0].name +
-                                                                "Latitude = " + result.data[0].latitude + 
-                                                                "\nLongitude = " + result.data[0].longitude))
-                                    .catch(error => console.log('error', error));
+                                    .then(result => [result.data[0].name, result.data[0].latitude, result.data[0].longitude])
                             });
                         });
-        console.log(markets);
+        return markets;
+    }
+
+    const fetchData = () => {
+        var list = []
+        const handleGeolocationSuccess = (position) => {
+            const { latitude, longitude } = position.coords;
+            RESTresponse().then(element => 
+                element.map(promise => promise.then(x => [x[0], calcDistance(latitude, longitude, x[1], x[2])]))
+                    .filter(promise => promise.then(x => x[1] < 100)))
+            .then(fList => list = fList);
+        };
+        
+        const handleGeolocationError = (error) => {
+           console.log("Geolocation error:", error);
+        };
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+              handleGeolocationSuccess,
+              handleGeolocationError
+            );
+        } else {
+            console.log("Geolocation is not supported by this browser.");
+        }
+        return list;
     }
 
     return <div>
         <h1>
-            This is to test the GeoCode API
+            This is to test the BackEnd API
         </h1>
         <h2>
             <button onClick={fetchData}>
