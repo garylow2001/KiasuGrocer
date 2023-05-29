6c1e958947a18195183990a007777682cce2ccdf
LicongHuang <alexliconghuang@gmail.com>
2023-05-29 15:34:52 +0800

    Server done

--- kiasu_grocer/src/pages/OutputDoubles.js
+++ kiasu_grocer/src/pages/OutputDoubles.js
@@ -2,7 +2,7 @@
 
 export function CalculatePairDouble(input1, input2) {
   // Perform calculations or operations to generate the output
-  const output = input1 * input2 * 2;
+  const output = Math.sqrt(input1 * input1 +  input2 * input2);
 
   // Return the output
   return output;
