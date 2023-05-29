ac886c67de46c8748688f2033378292f12acbd6e
LicongHuang <alexliconghuang@gmail.com>
2023-05-29 22:04:09 +0800

    Currently working

---
+++ kiasu_grocer/src/pages/NewProductForm.js
@@ -0,0 +1,138 @@
+import React, { useState } from 'react';
+//import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
+
+function ProductForm() {
+  const { id } = useParams();
+  const [productName, setProductName] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [expiryDate, setExpiryDate] = useState('');
+  const [image, setImage] = useState(null);
+  const [discount, setDiscount] = useState('0');
+
+  const handleProductNameChange = (event) => {
+    setProductName(event.target.value);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const handlePriceChange = (event) => {
+    setPrice(event.target.value);
+  };
+
+  const handleQuantityChange = (event) => {
+    setQuantity(event.target.value);
+  };
+      // Create a FormData object to send the form data
+      const formData = new FormData();
+      formData.append('name', productName);
+      formData.append('description', description);
+      formData.append('price', price);
+      formData.append('quantity', quantity);
+      formData.append('expiry_date', expiryDate);
+      formData.append('image', image);
+      formData.append('discount', discount);
+
+  const handleExpiryDateChange = (event) => {
+    setExpiryDate(event.target.value);
+  };
+
+  const handleImageChange = (event) => {
+    setImage(event.target.files[0]);
+  };
+
+  const handleDiscountChange = (event) => {
+    setDiscount(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const payload = {
+        vendor_id: id,
+        name: productName,
+        description: description,
+        price: price,
+        quantity: quantity,
+        expiry_date: expiryDate,
+        image: image,
+        discount: discount,
+      };
+
+      // Send the POST request to the database URL
+      const response = await fetch(
+        'http://localhost:5000/api/products/create', 
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(payload),
+        })
+
+      if (response.ok) {
+        // Handle the successful response, e.g., show a success message
+        console.log('Product created successfully!');
+        // Reset form fields
+        setProductName('');
+        setDescription('');
+        setPrice('');
+        setQuantity('');
+        setExpiryDate('');
+        setImage(null);
+        setDiscount('0');
+      } else {
+        // Handle the error response, e.g., show an error message
+        console.log('Failed to create product.');
+      }
+    } catch (error) {
+      // Handle any network or other errors
+      console.log('Error:', error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Product Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Product Name:
+          <input type="text" value={productName} onChange={handleProductNameChange} />
+        </label>
+        <label>
+          Description:
+          <textarea value={description} onChange={handleDescriptionChange}></textarea>
+        </label>
+        <label>
+          Price:
+          <input type="number" value={price} onChange={handlePriceChange} />
+        </label>
+        <label>
+          Quantity:
+          <input type="number" value={quantity} onChange={handleQuantityChange} />
+        </label>
+        <label>
+          Expiry Date:
+          <input type="date" value={expiryDate} onChange={handleExpiryDateChange} />
+        </label>
+        <label>
+          Image:
+          <input type="file" onChange={handleImageChange} />
+        </label>
+        <label>
+          Discount:
+          <input type="number" value={discount} onChange={handleDiscountChange} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default ProductForm;
+
---
+++ kiasu_grocer/src/pages/NewVendorForm.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const NewVendorForm = () => {
+  const 
